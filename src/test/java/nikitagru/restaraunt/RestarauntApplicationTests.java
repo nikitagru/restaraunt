3c0c6bf6396e3febb7d724ae18af54066369af17
nikitagru <grunix123@gmail.com>
2021-12-08 20:04:50 +0500

    Initial commit

---
+++ src/test/java/nikitagru/restaraunt/RestarauntApplicationTests.java
@@ -0,0 +1,13 @@
+package nikitagru.restaraunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestarauntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
